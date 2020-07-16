c0a52a1f55251cdd17ab9ed13dd38fce852ffcb3
PhamThanhHai <pthai076@gmail.com>
2020-07-16 21:14:15 +0700

    init project

---
+++ DesignPatterns/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DesignPatterns
+//
+//  Created by HaiKaito on 7/16/20.
+//  Copyright © 2020 HaiKaito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
