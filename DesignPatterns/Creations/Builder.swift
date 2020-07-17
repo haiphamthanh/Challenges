aafa6aef0d94c7519d1c8f5ec4681f1475b51aa3
PhamThanhHai <pthai076@gmail.com>
2020-07-17 15:27:09 +0700

    Builder patterns

---
+++ DesignPatterns/Creations/Builder.swift
@@ -0,0 +1,37 @@
+//
+//  Builder.swift
+//  DesignPatterns
+//
+//  Created by HaiKaito on 7/16/20.
+//  Copyright © 2020 HaiKaito. All rights reserved.
+//
+
+import Foundation
+
+// B1: Define types
+enum ButtonType {
+    case rectangle
+    case ovan
+}
+enum SwitchType {
+    case radio
+    case combobox
+}
+
+// B2: Create class
+class Machine {
+    // B3: Create properties
+    private var buttonType: ButtonType
+    private var switchTyle: SwitchType
+    
+    // B4: Make BUILDER function with default values
+    init(btnType: ButtonType = .rectangle, swType: SwitchType = .radio) {
+        self.buttonType = btnType
+        self.switchTyle = swType
+    }
+    
+    // Others functions
+    func description() -> String {
+        return "\(buttonType) \(switchTyle)"
+    }
+}
