38d5f4e74ab5ca879fa100708bb61dc3d5fc7747
PhamThanhHai <pthai076@gmail.com>
2020-07-18 13:09:40 +0700

    Adapter pattern

--- DesignPatterns/Structural/Adapter.swift
+++ DesignPatterns/Structural/Adapter.swift
@@ -19,38 +19,64 @@ protocol Receiver {
 
 // B1: Send message
 class VietNameseClient {
-    private let receiver: Receiver
-    init(receiver: Receiver) {
-        self.receiver = receiver
+    // SOLVE B3: use translator instead of implementing
+    private let translator: Sender
+    init(translator: Sender) {
+        self.translator = translator
     }
 }
 extension VietNameseClient: Sender {
     func send(words: String) {
-        receiver.receive(words: words)
+        // SOLVE B4: use translator instead of directly using receiver
+        print("Nguoi viet nam noi... \(words)")
+        return translator.send(words: words)
     }
 }
+
+// SOLVE B1: Make translator (Adapter class)
+class JPTranslator {
+    private let receiver: Receiver
+    
+    init(receiver: Receiver) {
+        self.receiver = receiver
+    }
+    
+    private func translate(words: String) -> String {
+        return "こんにちは"
+    }
+}
+extension JPTranslator: Sender {
+    func send(words: String) {
+        let newWords = translate(words: words)
+        
+        print("I would like to translate from \(words) to \(newWords)")
+        
+        // SOLVE B2: Adapter with new value
+        return receiver.receive(words: newWords)
+    }
+}
+
 
 // B2: Receive message
 class JapanseClient {
 }
 extension JapanseClient: Receiver {
     func receive(words: String) {
-        // PROBLEM: What??? I don't know english
-        print("Retrieving words from client ... \n \(words)")
+        print("Japanse Client was recieved ... \(words)")
     }
 }
 
 // Final class
 class Adapter {
     func main() -> Int {
-        
         // PROBLEM: I want to send vietnamese message to Japanese client
         // But I don't know japanese language
         // Solve: I need a translator who knows japanese language
         let jpanClient = JapanseClient()
-        let vnClient = VietNameseClient(receiver: jpanClient)
-        vnClient.send(words: "Hello")
+        let translator = JPTranslator(receiver: jpanClient)
+        let vnClient = VietNameseClient(translator: translator)
         
+        vnClient.send(words: "Xin Chao")
         return 1
     }
 }
