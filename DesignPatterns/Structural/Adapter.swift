476f00b90e80106d0befbd76d3a64509b07d53eb
PhamThanhHai <pthai076@gmail.com>
2020-07-18 12:36:15 +0700

    [Adapter] send raw message

--- DesignPatterns/Structural/Adapter.swift
+++ DesignPatterns/Structural/Adapter.swift
@@ -8,13 +8,48 @@
 
 import Foundation
 
+// B3: Action in vietnamese target
+protocol Sender {
+    func send(words: String)
+}
 
+protocol Receiver {
+    func receive(words: String)
+}
+
+// B1: Send message
+class VietNameseClient {
+    private let receiver: Receiver
+    init(receiver: Receiver) {
+        self.receiver = receiver
+    }
+}
+extension VietNameseClient: Sender {
+    func send(words: String) {
+        receiver.receive(words: words)
+    }
+}
+
+// B2: Receive message
+class JapanseClient {
+}
+extension JapanseClient: Receiver {
+    func receive(words: String) {
+        // PROBLEM: What??? I don't know english
+        print("Retrieving words from client ... \n \(words)")
+    }
+}
 
 // Final class
 class Adapter {
     func main() -> Int {
         
-        // PROBLEM:
+        // PROBLEM: I want to send vietnamese message to Japanese client
+        // But I don't know japanese language
+        // Solve: I need a translator who knows japanese language
+        let jpanClient = JapanseClient()
+        let vnClient = VietNameseClient(receiver: jpanClient)
+        vnClient.send(words: "Hello")
         
         return 1
     }
