288ea0e72f4f5ff09ed8c649d6b416bc598b811f
PhamThanhHai <pthai076@gmail.com>
2020-07-18 11:35:41 +0700

    [Mod] refactor folder name

---
+++ DesignPatterns/Structural/Adapter.swift
@@ -0,0 +1,21 @@
+//
+//  Adapter.swift
+//  DesignPatterns
+//
+//  Created by HaiKaito on 7/18/20.
+//  Copyright © 2020 HaiKaito. All rights reserved.
+//
+
+import Foundation
+
+
+
+// Final class
+class Adapter {
+    func main() -> Int {
+        
+        // PROBLEM:
+        
+        return 1
+    }
+}
