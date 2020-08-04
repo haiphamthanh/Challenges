41ebdfc97cb2137301464b7a9f7f3180f6a33486
PhamThanhHai <pthai076@gmail.com>
2020-08-04 08:43:50 +0700

    [Challenge] find and connect to the opponent

--- DesignPatterns/Section_3/VCChapter13.swift
+++ DesignPatterns/Section_3/VCChapter13.swift
@@ -43,5 +43,31 @@ class VCChapter13: BaseViewControllerSection03 {
 		// Move left to right and Move right to left then reverse all of them
 		myAvatar.bounceOff(point: rightBouncePoint, morphSize: morphSize)
 		opponentAvatar.bounceOff(point: leftBouncePoint, morphSize: morphSize)
+		
+		delay(seconds: 4.0, completion: foundOpponent)
+	}
+	
+	func foundOpponent() {
+		status.text = "Connecting..."
+		
+		opponentAvatar.image = UIImage(named: "avatar-2")
+		opponentAvatar.name = "Ray"
+		
+		delay(seconds: 4.0, completion: connectedToOpponent)
+	}
+	
+	func connectedToOpponent() {
+		myAvatar.shouldTransitionToFinishedState = true
+		opponentAvatar.shouldTransitionToFinishedState = true
+		
+		delay(seconds: 1.0, completion: completed)
+	}
+	
+	func completed() {
+		status.text = "Ready to play"
+		UIView.animate(withDuration: 0.2) {
+			self.vs.alpha = 1.0
+			self.searchAgain.alpha = 1.0
+		}
 	}
 }
